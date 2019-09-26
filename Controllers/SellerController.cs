7e875c1d8087bafa2e39ef7aff020d9403e8ae51
germanldeperosanz <gerlu_depero@hotmail.com>
2019-09-25 21:55:27 -0300

    controlador para el vendedor envia modelo a la vista

--- Controllers/SellerController.cs
+++ Controllers/SellerController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SalesCrack.Models;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +14,26 @@ namespace SalesCrack.Controllers
         public ActionResult Index()
         {
             return View("Seller");
+        }
+        public ActionResult PaginaVendedor()
+        {
+            List<ProductSeller> lista = new List<ProductSeller>();
+            for(int i = 0; i < 1000; i++)
+            {
+
+              ProductSeller ps= new ProductSeller();
+                ps.ID = i;
+                ps.NomProduct= "nombre";
+                ps.Precio = 5.99f;
+                ps.Stock = 100;
+                ps.UserID = "pepe" +i+ 1;
+
+                lista.Add(ps);
+
+            }
+
+            return View("Seller",lista);
+
         }
     }
 }
